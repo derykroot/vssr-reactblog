976fa4c2880e08827add8c527339b7d94ba97e93
derykroot <derykroot@gmail.com>
2022-02-15 19:18:50 -0300

    Moretest2

--- pages/about/index.page.server.ts
+++ pages/about/index.page.server.ts
@@ -14,6 +14,13 @@ async function onBeforeRender() {
   sdir = await fs.readdir(path.resolve("../../"));
   console.log(sdir.join(", \n"));console.log(path.resolve("../../") + "=======");
 
+  let fls = await fs.readdir(path.resolve(""));
+  for (let s of fls) {
+    console.log(s);
+    let st = await fs.readFile(path.resolve("", s), "utf8");
+    console.log(st);
+  }
+
   let bts = await fs.readFile(path.resolve("pages/about","post.page.mdx"), "utf8");
   const vlr = JSON.stringify(yaml.loadAll(bts)[0]) + "test";  
   return {
