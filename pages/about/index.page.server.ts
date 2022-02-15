a0b6a06f6f0389e7eb3b5ea502288d3acb1d7327
derykroot <derykroot@gmail.com>
2022-02-15 19:04:28 -0300

    Moretest

--- pages/about/index.page.server.ts
+++ pages/about/index.page.server.ts
@@ -5,8 +5,15 @@ import yaml from 'js-yaml'
 export { onBeforeRender }
 
 async function onBeforeRender() {
-  console.log("dir: " + path.resolve(""));
+  console.log("dir: " + path.resolve("../../"));
   console.log("dirfile: " + path.resolve("pages/about","post.page.mdx"));
+  let sdir = await fs.readdir(path.resolve(""));
+  console.log(sdir.join(", \n"));console.log(path.resolve("") + "*******");
+  sdir = await fs.readdir(path.resolve("../"));
+  console.log(sdir.join(", \n"));console.log(path.resolve("../") + "*******");
+  sdir = await fs.readdir(path.resolve("../../"));
+  console.log(sdir.join(", \n"));console.log(path.resolve("../../") + "=======");
+
   let bts = await fs.readFile(path.resolve("pages/about","post.page.mdx"), "utf8");
   const vlr = JSON.stringify(yaml.loadAll(bts)[0]) + "test";  
   return {
