3618a77b336ff94f9da2ab4de8935552bce0e640
derykroot <derykroot@gmail.com>
2022-02-15 18:33:09 -0300

    adjust

--- pages/about/index.page.server.ts
+++ pages/about/index.page.server.ts
@@ -4,7 +4,7 @@ import yaml from 'js-yaml'
 export { onBeforeRender }
 
 async function onBeforeRender() {
-  let bts = await fs.readFile("./pages/about/post.page.mdx", "utf8");
+  let bts = await fs.readFile("/pages/about/post.page.mdx", "utf8");
   const vlr = JSON.stringify(yaml.loadAll(bts)[0]) + "test";
   return {
     pageContext: {
